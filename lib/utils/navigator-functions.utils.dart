ddf4ef59731a58df772875b17f5eb7e9b66f722b
Almuhannad <henno.noman@gmail.com>
2020-08-16 16:04:56 +0300

    add pushAndRemoveUntil

--- lib/utils/navigator-functions.utils.dart
+++ lib/utils/navigator-functions.utils.dart
@@ -4,13 +4,27 @@ Future<T> pushNewScreen<T extends Object>(
   BuildContext context, {
   @required Widget screen,
   bool withNavBar,
-  bool defaultNavBar=false,
   PageTransitionAnimation pageTransitionAnimation =
       PageTransitionAnimation.cupertino,
   PageRoute customPageRoute,
 }) {
   if (withNavBar == null) {
-    withNavBar = defaultNavBar;
+    withNavBar = true;
+  }
+  return Navigator.of(context, rootNavigator: !withNavBar).push(
+      customPageRoute ??
+          getPageRoute(pageTransitionAnimation, enterPage: screen));
+}
+Future<T> pushAndRemoveUntil<T extends Object>(
+  BuildContext context, {
+  @required Widget screen,
+  bool withNavBar,
+  PageTransitionAnimation pageTransitionAnimation =
+      PageTransitionAnimation.cupertino,
+  PageRoute customPageRoute,
+}) {
+  if (withNavBar == null) {
+    withNavBar = false;
   }
   return Navigator.of(context, rootNavigator: !withNavBar).push(
       customPageRoute ??
